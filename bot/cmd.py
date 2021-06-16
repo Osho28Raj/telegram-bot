b973f34628efb6012677826c06f6831042b44aac
Osho Raj <65038390+Osho28Raj@users.noreply.github.com>
2021-06-16 08:44:46 +0530

    Update cmd.py

--- bot/cmd.py
+++ bot/cmd.py
@@ -1,4 +1,17 @@
-from telegram import botcommand as bot
+from typing import Any
+
+from telegram import TelegramObject
+
+
+class BotCommand(TelegramObject):
+    __slots__ = ('description', '_id_attrs', 'command')
+
+    def __init__(self, command: str, description: str, **_kwargs: Any):
+        self.command = command
+        self.description = description
+
+        self._id_attrs = (self.command, self.description)
+
 class _BotCommands:
     def __init__(self):
         self.owner = "owner"
@@ -11,8 +24,8 @@ info = _BotCommands()
 
 
 botcmds = [
-    bot(f"{info.owner}", "To get owner id"),
-    bot(f"{info.start}", "Introduction of the bot"),
-    bot(f"{info.gandu}", "Gali sunne ke liye"),
-    bot(f"{info.slap}", "Slaps you with random objects")
+    BotCommand(f"{info.owner}", "To get owner id"),
+    BotCommand(f"{info.start}", "Introduction of the bot"),
+    BotCommand(f"{info.gandu}", "Gali sunne ke liye"),
+    BotCommand(f"{info.slap}", "Slaps you with random objects")
 ]
