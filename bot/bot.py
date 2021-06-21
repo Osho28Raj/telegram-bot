a975f9745c2a5d5baa1c39dca4f843fdc6e96281
Osho Raj <rajosho08@gmail.com>
2021-06-21 16:29:38 +0530

    pokedex added

--- bot/bot.py
+++ bot/bot.py
@@ -4,7 +4,9 @@ import random
 from telegram import *
 from telegram.ext import *
 import random
-import reply as reply
+import requests
+import reply
+import cmd
 token = os.environ.get("TOKEN")
 bot = Bot(token)
 pic = "https://telegra.ph/file/dab7aa0e02fe075f35dff.jpg"
@@ -21,6 +23,112 @@ def start(update:Update,context:CallbackContext):
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 start_value = CommandHandler("start", start)
+dispatcher.add_handler(start_value)
+#===============================================
+
+#===================/poke======================
+def poke(update:Update,context:CallbackContext):
+    chat_id = update.effective_chat.id
+    pokemon = update.message.text
+    pokemon = pokemon.replace("/poke ", "")
+    move = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+    status = move.ok
+    if status == True:
+        rw = f"https://some-random-api.ml/pokedex?pokemon={pokemon}"
+        img = requests.get(f"https://api.pokemontcg.io/v1/cards?name={pokemon}")
+        lol = img.json()
+        img_url = lol["cards"][0]["imageUrlHiRes"]
+        r = requests.get(rw)
+        a = r.json()
+        name = a["name"].upper()
+        typ = a["type"]
+        species = a["species"]
+        abilities = a["abilities"]
+        height = a["height"]
+        weight = a["weight"]
+        esatge = r.json()["family"]["evolutionStage"]
+        try:
+            weaknesses = lol["cards"][0]["weaknesses"][0]["type"]
+        except BaseException:
+            weaknesses = None
+        l = r.json()["family"]["evolutionLine"]
+        # ambiguous variable name 'l' flake8(E741)
+        if not l:
+            line = "None"
+        else:
+            line = ", ".join(map(str, l))
+        gen = a["generation"]
+        try:
+            move1 = move.json()["moves"][0]["move"]["name"]
+        except IndexError:
+            move1 = None
+        try:
+            move2 = move.json()["moves"][1]["move"]["name"]
+        except IndexError:
+            move2 = None
+        try:
+            move3 = move.json()["moves"][2]["move"]["name"]
+        except IndexError:
+            move3 = None
+        try:
+            move4 = move.json()["moves"][3]["move"]["name"]
+        except IndexError:
+            move4 = None
+        try:
+            move5 = move.json()["moves"][4]["move"]["name"]
+        except IndexError:
+            move5 = None
+        try:
+            move6 = move.json()["moves"][5]["move"]["name"]
+        except IndexError:
+            move6 = None
+        try:
+            move7 = move.json()["moves"][6]["move"]["name"]
+        except IndexError:
+            move7 = None
+        description = a["description"]
+        typ = ", ".join(map(str, typ))
+        Stats = a["stats"]
+        species = ", ".join(map(str, species))
+        abilities = ", ".join(map(str, abilities))
+        cap = f"""\n\n
+**NAME** : `{name}`
+**TYPE** : `{typ}`
+**SPECIES** : `{species}`
+**Evolution Line** : `{line}`
+**Evolution Stage** : `{esatge}`
+**Generation** : `{gen}`
+
+**ABILITIES** : `{abilities}`
+**WEAKNESSES** :`{weaknesses}`
+**HEIGHT** : `{height}`
+**WEIGHT** : `{weight}`
+
+    **Stats**                               **Moves**
+**Hp**      : `{Stats['hp']}`               `(1){move1}`
+**Attack**  : `{Stats['attack']}`           `(2){move2}`
+**Defense** : `{Stats['defense']}`          `(3){move3}`
+**S. Att**  : `{Stats['sp_atk']}`           `(4){move4}`
+**S. Def**  : `{Stats['sp_def']}`           `(5){move5}`
+**Speed**   : `{Stats['speed']}`            `(6){move6}`
+**Total**   : `{Stats['total']}`            `(7){move7}`
+
+**DESCRIPTION** : `{description}`
+"""
+        bot.sendPhoto(
+            chat_id,
+            img_url,
+            cap,
+            parse_mode=telegram.ParseMode.MARKDOWN,
+        )
+    
+    else:
+        bot.send_message(
+            chat_id=chat_id,
+            text="`Invalid Input`\nExample : `/poke pikachu`",
+            parse_mode=telegram.ParseMode.MARKDOWN,
+        )
+start_value = CommandHandler("poke", poke)
 dispatcher.add_handler(start_value)
 #===============================================
 
--- bot/cmd.py
+++ bot/cmd.py
@@ -6,6 +6,7 @@ class _BotCommands:
         self.start = "start"
         self.gandu = "gandu"
         self.slap = "slap"
+        self.poke = "poke"
 
 
 info = _BotCommands()
@@ -15,5 +16,6 @@ botcmds = [
     bot(f"{info.owner}", "To get owner id"),
     bot(f"{info.start}", "Introduction of the bot"),
     bot(f"{info.gandu}", "Gali sunne ke liye"),
-    bot(f"{info.slap}", "Slaps you with random objects")
+    bot(f"{info.slap}", "Slaps you with random objects"),
+    bot(f"{info.poke}", "Gives you detail about a pokemon")
 ]
